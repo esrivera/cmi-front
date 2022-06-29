00342e635cacf17e6149686c65d332ecad447bdc
Santiago Rivera <esrivera4@espe.edu.ec>
2022-06-29 11:08:33 -0500

    add header token

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,25 @@ import axios from "axios";
 //const API_URL = "http://localhost:9012";
 const API_URL = "https://appcmi.herokuapp.com";
 
-export const clientPublic = axios.create({
-  baseURL: `${API_URL}`,
-});
+// export const clientPublic = axios.create({
+//   baseURL: `${API_URL}`,
+// });
 
 //axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
+
+const defaultOptions = {
+  baseURL: `${API_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+// Create instance
+export const clientPublic = axios.create(defaultOptions);
+
+// Set the AUTH token for any request
+clientPublic.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
+});
